35305631ea01e5d9ad276ce1067c6bdd433fb062
yerkebulan.omar <yerkebulan.omar@gmail.com>
2023-02-12 13:25:54 +0600

    added button to task menu

--- src/Components/TaskList/index.js
+++ src/Components/TaskList/index.js
@@ -4,7 +4,9 @@ import "./styles.css";
 export default function Task() {
   return (
     <div className="task">
-      <img className="task-dots" src={TASK_DOTS} alt="task menu" />
+      <button className="task-dots-button">
+        <img className="task-dots-img" src={TASK_DOTS} alt="task menu" />
+      </button>
       <input type="checkbox" id="task1" name="task1" value="task1" />
       <label className="task-text" for="task1">
         Write Essay
