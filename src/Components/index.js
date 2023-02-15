41dfa4ea85677efd26c70a3a42d05816e710e5e5
yerkebulan.omar <yerkebulan.omar@gmail.com>
2023-02-16 05:19:42 +0600

    finished sorting for done tasks except for Trash button

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,38 +1,38 @@
 import "./styles.css";
 import PLUS_SIGN from "./Images/Plus Math.svg";
-import Task from "./TaskList";
 import { useState } from "react";
+import TASK_DOTS from "./Images/Vector.svg";
+let tasks = [
+  {
+    id: 0,
+    content: "write an essay",
+    type: "To Do",
+  },
+  {
+    id: 1,
+    content: "read the book",
+    type: "To Do",
+  },
+  {
+    id: 2,
+    content: "go to gym",
+    type: "Done",
+  },
+  {
+    id: 3,
+    content: "call mom",
+    type: "To Do",
+  },
+  {
+    id: 4,
+    content: "finish the task",
+    type: "Trash",
+  },
+];
 
 export default function Main() {
-  const tasks = [
-    {
-      id: 0,
-      toDo: "write an essay",
-      topic: "To Do",
-    },
-    {
-      id: 1,
-      toDo: "read the book",
-      topic: "Trash",
-    },
-    {
-      id: 2,
-      toDo: "go to gym",
-      topic: "To Do",
-    },
-    {
-      id: 3,
-      toDo: "call mom",
-      topic: "Done",
-    },
-    {
-      id: 4,
-      toDo: "finish the task",
-      topic: "Done",
-    },
-  ];
-
-  const [category, setCategory] = useState("To Do");
+  const [items, setItems] = useState(tasks);
+  const [type, setType] = useState("To Do");
 
   const [isActiveToDo, setIsActiveToDo] = useState(true);
   const [isActiveDone, setIsActiveDone] = useState(false);
@@ -54,11 +54,31 @@ export default function Main() {
     setIsActiveTrash(true);
   };
 
+  const handleCheck = (keyFromCheck) => {
+    // console.log(keyFromCheck);
+    const index = items.findIndex((item) => item.id === keyFromCheck);
+    const oldObject = items[index];
+    // console.log(oldObject);
+    const newObject = { ...oldObject, checked: !oldObject.checked };
+    const leftPart = items.slice(0, index);
+    const rightPart = items.slice(index + 1, items.length);
+    const newItems = [...leftPart, newObject, ...rightPart];
+    setItems(newItems);
+  };
+
   const [filter, setFilter] = useState("To Do");
 
-  const filteredData = tasks.filter((item) => {
-    return item.topic === filter;
-  });
+  const handleStatus = (typeFromButton) => {
+    setType(typeFromButton);
+  };
+
+  const filteredData = items.filter((item) =>
+    type === "To Do"
+      ? !item.checked
+      : type === "Done"
+      ? item.checked
+      : type === "Trash"
+  );
 
   return (
     <div className="main">
@@ -73,9 +93,9 @@ export default function Main() {
               color: isActiveToDo ? "white" : "",
             }}
             onClick={() => {
-              setCategory("To Do");
               selectToDo();
               setFilter("To Do");
+              handleStatus("To Do");
             }}>
             To Do
           </button>
@@ -88,9 +108,9 @@ export default function Main() {
               color: isActiveDone ? "white" : "",
             }}
             onClick={() => {
-              setCategory("Done");
               selectDone();
               setFilter("Done");
+              handleStatus("Done");
             }}>
             Done
           </button>
@@ -103,9 +123,9 @@ export default function Main() {
               color: isActiveTrash ? "white" : "",
             }}
             onClick={() => {
-              setCategory("Trash");
               selectTrash();
               setFilter("Trash");
+              handleStatus("Trash");
             }}>
             Trash
           </button>
@@ -117,11 +137,23 @@ export default function Main() {
         </div>
       </div>
       <div className="window-title">
-        <h4 className="title-text">{category}</h4>
+        <h4 className="title-text">{filter}</h4>
       </div>
       <div className="task-list">
-        {filteredData.map((item, index) => (
-          <Task key={index} {...item} />
+        {filteredData.map((item) => (
+          <div className="task" key={item.id}>
+            <button className="task-dots-button">
+              <img className="task-dots-img" src={TASK_DOTS} alt="task menu" />
+            </button>
+            <input
+              type="checkbox"
+              checked={item.checked}
+              onChange={() => {
+                handleCheck(item.id);
+              }}
+            />
+            <label className="task-text">{item.content}</label>
+          </div>
         ))}
       </div>
     </div>
